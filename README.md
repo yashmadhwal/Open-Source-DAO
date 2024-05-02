1663311008503573bb62ec77f800d7b75403c2f3
Igor Maksimov <igormaximow@gmail.com>
2024-05-02 16:48:49 +0300

    add readme contents

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Open-Source-DAO
+
+## Setup
+
+1. Go to `contract`
+2. Install the dependencies: `npm i`
+3. Build: `npx hardhat compile && npx hardhat typechain`
+4. Go to `frontend`
+5. Install the dependencies: `npm i`
+6. Run the dApp: `npm run dev`
--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -7,26 +7,27 @@ import '@typechain/hardhat';
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
 
-const BSC_TESTNET_PRIVATE_KEY = vars.get("BSC_TESTNET_PRIVATE_KEY");
-const BSC_TESTNET_API_KEY = vars.get("BSC_TESTNET_API_KEY");
-const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY")
+// Needed for deployment, but we deploy via frontend, currently
+// const BSC_TESTNET_PRIVATE_KEY = vars.get("BSC_TESTNET_PRIVATE_KEY");
+// const BSC_TESTNET_API_KEY = vars.get("BSC_TESTNET_API_KEY");
+// const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY")
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  networks: {
-    bsc_testnet: {
-      url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
-      accounts: [BSC_TESTNET_PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: {
-      bscTestnet: BSCSCAN_API_KEY
-    }
-  },
-  bscscan: {
-    "apiKey": BSC_TESTNET_API_KEY
-  },
+  // networks: {
+  //   bsc_testnet: {
+  //     url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
+  //     accounts: [BSC_TESTNET_PRIVATE_KEY],
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: {
+  //     bscTestnet: BSCSCAN_API_KEY
+  //   }
+  // },
+  // bscscan: {
+  //   "apiKey": BSC_TESTNET_API_KEY
+  // },
   namedAccounts: {
     deployer: {
         default: 0, // here this will by default take the first account as deployer
